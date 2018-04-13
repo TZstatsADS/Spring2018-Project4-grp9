7c2b000070e0d1a087a5c6ee732979f1957ce7b3
Kent Kang <kentkang@dyn-160-39-148-41.dyn.columbia.edu>
2018-04-13 19:07:18 -0400

    neb select & predict

--- lib/neighbor&predict.R
+++ lib/neighbor&predict.R
@@ -10,59 +10,86 @@ neighbors.select <- function(sim_weights, N = 50){
   ##                  N: amount of best neighbors
   ## Output: A list of neighbors's index for each users
   top.neighbors = list()
-  coverage = 0
-  for(i in 1:nrow(simweights)){
-    vec1 = simweights[i,]
-    index_ = which((abs(vec1) > thresholding) & (vec1 != 1))
-    top.neighbors[[i]] = index_
-    coverage = coverage + length(top.neighbors[[i]])
+  coverage = c()
+  for(i in 1:nrow(sim_weights)){
+    vec1 = sim_weights[i,]
+    sort_abs = sort(abs(vec1),decreasing = T)
+    index = which((abs(vec1) >= sort_abs[1+N]) & (vec1 != 1))
+    top.neighbors[[i]] = index
+    coverage = union(coverage,index)
   }
+  coverage = round(length(coverage)/nrow(sim_weights),4)*100
+  
+  print(paste0("The coverage percent is ", coverage,"%"))
   return(top.neighbors)
 }
 
-pearson.var.MS = var_weight_MS * pearson_weight_MS
-simweights=pearson.var.MS
-pearson.var.MS.pred = pred.matrix.ms(simweights =pearson.var.MS, top.neighbors =pearson.var.MS.neighbor)
 
+## Make predictions - Average z-score
 
-pred.matrix.ms <- function(testdat = MS_test, traindat = MS_train, simweights, top.neighbors){
-  #testdat = MS_test
-  #traindat = MS_train
-  #simweights = pearson_weight_MS
-  #top.neighbors = results_MS
-  prediction.matrix = matrix(0, ncol = ncol(traindat), nrow = nrow(traindat))
-  avg.ratings = apply(traindat, 1, mean, na.rm = T)
-  test.col = colnames(testdat)
-  test.row = rownames(testdat)
-  # neighbor.avg <- apply(traindat,1,mean,na.rm=T)
-  # neighbor.weights = try[user,top.neighbors]
-  for(i in 1:nrow(traindat)){
-    #train.col = colnames(testdat)[which(!is.na(testdat[i,]))]
-    #train.loc = traindat[i,train.col]
-    neighbor.weights = simweights[i,top.neighbors[[i]]]
-    neighbor.ratings = traindat[top.neighbors[[i]], ]
-    # neighbor.avg = ifelse(length(top.neighbors[[i]]) <2, mean(traindat[top.neighbors[[i]], !test.loc[i,]]),apply(traindat[top.neighbors[[i]], ],1,mean,na.rm= T))
-    if(length(top.neighbors[[i]]) <2){
-      if(length(top.neighbors[[i]]) < 1){
-        prediction.matrix[i,] = avg.ratings[i]
-      }
-      else{prediction.matrix[i,] = avg.ratings[i] + (neighbor.ratings-avg.ratings[top.neighbors[[i]]]) * neighbor.weights / sum(neighbor.weights, na.rm = T)}
-    }
-    else{
-      prediction.matrix[i,] = avg.ratings[i] + apply((neighbor.ratings-avg.ratings[top.neighbors[[i]]]) * neighbor.weights, 2, sum, na.rm=T) / sum(neighbor.weights, na.rm = T)
-    }
-    
+predict.MS <- function(test = MS_test, train = MS_train,
+                       sim_weights, top.neighbors){
+  prediction.matrix = matrix(0, ncol = ncol(train), nrow = nrow(train))
+  avg.ratings = apply(train, 1, mean, na.rm = T)
+  sd.ratings = apply(train, 1, sd, na.rm = T)
+  test.col = colnames(test)
+  test.row = rownames(test)
+  ### Average z-score
+  for(i in 1:nrow(train)){
+    neighbor.weights = sim_weights[i,top.neighbors[[i]]]
+    neighbor.ratings = train[top.neighbors[[i]], ]
+    r_i = avg.ratings[i] 
+    sd_i = sd.ratings[i] 
+    weight_sum =sum(neighbor.weights, na.rm = T)
+    each_neighbor = ((neighbor.ratings-avg.ratings[top.neighbors[[i]]])/sd.ratings[top.neighbors[[i]]]) * neighbor.weights
+    prediction.matrix[i,] = r_i +  sd_i*apply(each_neighbor, 2, sum, na.rm=T) / weight_sum
   }
-  colnames(prediction.matrix) = colnames(traindat)
-  rownames(prediction.matrix) = rownames(traindat)
+  colnames(prediction.matrix) = colnames(train)
+  rownames(prediction.matrix) = rownames(train)
   prediction1 = prediction.matrix[test.row,test.col]
   return(prediction1)
 }
 
-pearson.var.movie = mat_variance_weight_movie * pearson_weight
-pearson.var.movie.neighbor =  neighbors.select(pearson.var.movie)
-pearson.var.movie.pred = pred.matrix.movie(simweights =pearson.var.movie,top.neighbors = pearson.var.movie.neighbor)
+## Prediction
+predict.Movie <- function(test = Movie_test, train = Movie_train, 
+                          sim_weights, top.neighbors){
+  prediction.matrix = matrix(NA, ncol = ncol(test), nrow = nrow(test))
+  test.loc = is.na(test)
+  avg.ratings = apply(train, 1, mean, na.rm = T)
+  sd.ratings = apply(train, 1, sd, na.rm = T)
+  for(i in 1:nrow(train)){
+    train.col = colnames(test)[which(!is.na(test[i,]))]
+    neighbor.weights = sim_weights[i,top.neighbors[[i]]]
+    neighbor.ratings = train[top.neighbors[[i]], train.col]
+    ### Average z-score
+    r_i = avg.ratings[i] 
+    sd_i = sd.ratings[i] 
+    weight_sum =sum(neighbor.weights, na.rm = T)
+    each_neighbor = ((neighbor.ratings-avg.ratings[top.neighbors[[i]]])/sd.ratings[top.neighbors[[i]]]) * neighbor.weights
+    prediction.matrix[i,!test.loc[i,]] = round((r_i +  sd_i* apply(each_neighbor, 2, sum, na.rm=T)/weight_sum),0)
+    
+  }
+  return(prediction.matrix)
+}
 
-pearson.var.MS = var_weight_MS * pearson_weight_MS
-pearson.var.MS.neighbor = neighbors.select(pearson.var.MS)
+############################################################
 
+### Example: Pearson + Varaiance Weighting + Best N neighbor + Average z-score predict
+
+## Movie dataset
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/Similarity_Weight/weight_pearson_Movie.RData")
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/variance_weight/var_weight_Movie.RData")
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/data/Movie_test.RData")
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/data/Movie_train.RData")
+pearson_var_Movie = var_weight_Movie * weight_pearson_Movie
+pearson_var_Movie_neighbor =  neighbors.select(pearson_var_Movie, N=20)
+pearson_var_Movie_pred = predict.Movie(sim_weights = pearson_var_Movie,top.neighbors = pearson_var_Movie_neighbor)
+
+## MS dataset
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/Similarity_Weight/weight_pearson_MS.RData")
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/variance_weight/var_weight_MS.RData")
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/data/MS_test.RData")
+load("~/Documents/GitHub/Spring2018-Project4-grp9/output/data/MS_train.RData")
+pearson_var_MS = var_weight_MS * weight_pearson_MS
+pearson_var_MS_neighbor = neighbors.select(pearson_var_MS,N=20)
+pearson_var_MS_pred = predict.MS(sim_weights =pearson_var_MS, top.neighbors = pearson_var_MS_neighbor)
