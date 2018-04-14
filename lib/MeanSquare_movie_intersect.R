a3cc627e4391563e94cd899856e92249f565a474
Fangbing Liu <35517023+fangbingliu@users.noreply.github.com>
2018-04-14 13:39:29 -0400

    intersect

---
+++ lib/MeanSquare_movie_intersect.R
@@ -0,0 +1,42 @@
+### ADS Project 4 Spring 2018
+### Fangbing Liu
+### Memory-based 
+### Similarity Weights(MSD of movie data with intersection)
+
+n_intersect <- matrix(NA, nrow = nrow(Movie_train), ncol = nrow(Movie_train))
+for(i in 1:nrow(Movie_train)){
+  for(j in 1:nrow(Movie_train)){
+    r_i <- Movie_train[i,]
+    r_j <- Movie_train[j,]
+    n_intersect[i,j] <- length(intersect(which(!is.na(r_i)), which(!is.na(r_j))))
+  }
+}
+mean(n_intersect) # 45
+
+MSD_intersect <- function(data){
+  n <- nrow(data) # number of users
+  dissim <- matrix(NA, n, n)
+  rownames(dissim) <- rownames(data)
+  colnames(dissim) <- rownames(data)
+  
+  for(i in 1:n){
+    for(j in 1:n){
+      r_i <- data[i,]
+      r_j <- data[j,]
+      n_intersect <- length(intersect(which(!is.na(r_i)), which(!is.na(r_j))))
+      if(n_intersect > 60){
+        dissim[i,j] <- 0
+      }
+      else{
+        dissim[i,j] <- mean((r_i - r_j)^2, na.rm = T)
+      }
+      if(is.nan(dissim[i,j])){
+        dissim[i,j] = NA
+      }
+    }
+  }
+  sim <- round((max(dissim, na.rm = T) - dissim)/max(dissim, na.rm = T),4)
+  return(sim)
+}
+weight_MSD_Movie_intersect <- MSD_intersect(Movie_train)
+save(weight_MSD_Movie_intersect, file = "../output/Similarity_Weight/weight_MSD_Movie_intersect.RData")
