e58676ebd8dc23046eba4db8244056723a7f5089
Kent Kang <kentkang@KentdeMacBook-Pro.local>
2018-04-18 16:43:10 -0400

    var weight code

--- lib/var_weight.R
+++ lib/var_weight.R
@@ -5,6 +5,7 @@
 ## Variance Weighting
 get_v_i <- function(mat=Movie_train){
   var_vector <- apply(mat, 2, var, na.rm=TRUE)
+  var_vector[is.na(var_vector)] <- 0
   var_max <- max(var_vector, na.rm = TRUE)
   var_min <- min(var_vector, na.rm = TRUE)
   v_i <- (var_vector - var_min)/var_max
@@ -21,22 +22,56 @@ var_weight_assign <- function(i, j, v, mat=Movie_train){
 
 var_weight_matrix <- function(mat = Movie_train){
   n = dim(mat)[1]
+  n_items <- ncol(mat)
   mat_weight = matrix(1, nrow=n, ncol=n)
   v <- get_v_i(mat = mat)
-  for (i in 1:(n-1)){
+  #########################
+  v <- v/sum(v)
+  mat <- t(scale(t(mat)))
+  mat[is.na(mat)] <- 0
+  mat_v <- t(t(mat) * v)
+  mat_weight <- mat_v %*% t(mat)
+  V_correction <- matrix(NA, ncol=n,nrow=n)
+  for ( i in 1:n){
     print(i)
-    print(Sys.time())
-    for (j in (i+1):n){
-      weight <- var_weight_assign(i, j, v, mat = mat)
-      mat_weight[i, j] <- weight
-      mat_weight[j, i] <- weight
-    }
+    int_mask <- (mat[i,] * t(mat)) != 0
+    V_correction[i,] <- colSums(int_mask * v)
   }
+  mat_weight <- mat_weight/V_correction
+  #########################
   return(mat_weight)
 }
 
 #################################################################
 #################################################################
+MSD_var <- function(data){
+  n <- nrow(data) # number of users
+  dissim <- matrix(0, n, n)
+  rownames(dissim) <- rownames(data)
+  colnames(dissim) <- rownames(data)
+  v <- get_v_i(mat = data)
+  for(i in 1:(n-1)){
+    print(i)
+    print(Sys.time())
+    for(j in (i+1):n){
+      r_i <- data[i,]
+      r_j <- data[j,]
+      #index <- intersect(which(!is.na(r_i)), which(!is.na(r_j)))
+      dissim[i,j] <- weighted.mean((r_i - r_j)^2, w = v, na.rm = T)
+      
+      #weight <- sum(v[index]*dissim[i,j],na.rm = T)/sum(v[index])
+      
+      if(is.nan(dissim[i,j])){
+        dissim[i,j] = NA
+      }
+      #dissim[j,i] <- dissim[i,j]
+    }
+  }
+  dissim[lower.tri(dissim, diag = FALSE)] <- dissim[upper.tri(dissim, diag = FALSE)]
+  sim <- round((max(dissim, na.rm = T) - dissim)/max(dissim, na.rm = T),4)
+  return(sim)
+}
+
 
 ## Apply it to training data
 load("../output/data/Movie_train.RData")
@@ -52,3 +87,20 @@ var_weight_Movie <- var_weight_matrix(mat = Movie_train)
 var_weight_Movie[is.na(var_weight_Movie)] = 0
 var_weight_Movie <- round(variance_weight_Movie,3)
 save(var_weight_Movie, file = "../output/variance_weight/var_weight_Movie.RData")
+
+## Apply it to training data
+load("../output/data/Movie_train.RData")
+load("../output/data/MS_train.RData")
+
+MSD_var_weight_MS <- MSD_var(data = MS_train)
+MSD_var_weight_MS[is.na(MSD_var_weight_MS)] = 0
+MSD_var_weight_MS <- round(MSD_var_weight_MS,4)
+save(MSD_var_weight_MS, file = "~/Documents/GitHub/Spring2018-Project4-grp9/output/variance_weight/MSD_var_weight_MS.RData")
+
+
+MSD_var_weight_Movie <- MSD_var(data = Movie_train)
+MSD_var_weight_Movie[is.na(MSD_var_weight_Movie)] = 0
+MSD_var_weight_Movie <- round(MSD_var_weight_Movie,4)
+save(MSD_var_weight_Movie, file = "~/Documents/GitHub/Spring2018-Project4-grp9/output/variance_weight/MSD_var_weight_Movie.RData")
+
+
